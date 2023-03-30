6754631ccb6b87e424f3b6ed0b71a156f880e233
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-03-30 23:55:41 +0900

    Feat : 스프링 빈 조회
    1. 기본 조회
    2. 동일한 타입 둘 이상 시 조회 방법
    3. 상속관계 조회

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,24 +9,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.io.DataInputStream;
 
-
+@Configuration // AppConfig를 설정정보로 사용
 public class AppConfig {
-
+    @Bean // 스프링 컨테이너에 등록
     public MemberService memberService() {
         // 생성자 주입
         return new MemberServiceImpl(memberRepository());
     }
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
         /*할인 정책을 변경할 때에는 return 값만 바꿔주면 된다.*/
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();

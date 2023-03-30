0bf75b971efc64f4ec6d70e2fc6b52c8443cb81f
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-03-30 23:54:56 +0900

    Etc : 스프링으로 전환

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -1,14 +1,24 @@
 package hello.core;
 
 import hello.core.member.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
 //        MemberService memberService = new MemberServiceImpl(new MemoryMemberRepository());
-        MemberService memberService = appConfig.memberService();
+//        MemberService memberService = appConfig.memberService();
+
+        // ApplicationContext를 스프링 컨테이너라고 한다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
+        /*
+        - 메서드 명을 스프링 빈의 이름으로 사용됨.
+        - 스프링 컨테이너를 통해서 필요한 스프링 빈를 찾는다. applicationContext.getBean()을 통해 찾는다.
+         */
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);

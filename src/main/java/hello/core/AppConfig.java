fdc4aad16aaea3e573e2fbb647bce70d73772952
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-03-31 02:08:58 +0900

    Feat : @Configuration과 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -16,17 +16,41 @@ import java.io.DataInputStream;
 
 @Configuration // AppConfig를 설정정보로 사용
 public class AppConfig {
+
+    /*
+    @Bean memberSErvice -> new MemoryMemberRepository()
+    @Bean orderService -> new MemoryMemberRepository()
+    싱글톤 패턴이 깨진 것 같지만 같은 인스턴스를 참조함.
+     */
+
+    /* @Configuration을 사용하지 않았을 때
+    * call AppConfig.memberService
+    * call AppConfig.memberRepository
+    * call AppConfig.orderService
+    * call AppConfig.memberRepository
+    * call AppConfig.memberRepository
+    * */
+
+    /* @Configuration을 사용했을 때
+     * call AppConfig.memberService
+     * call AppConfig.memberRepository
+     * call AppConfig.orderService
+    * */
+
     @Bean // 스프링 컨테이너에 등록
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         // 생성자 주입
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     @Bean

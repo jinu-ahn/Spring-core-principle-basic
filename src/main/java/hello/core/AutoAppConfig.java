ce8883721f499911116e912a51e623171700a8d1
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-04-01 23:52:24 +0900

    Add : 탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -11,7 +11,10 @@ import org.springframework.context.annotation.FilterType;
         AppConfig, TestConfig 등 앞서 만들어두었던 설정 정보도 함께 등록되고, 실행되어 버린다.
         그렇기 때문에 @Configuration이 붙은 클래스는 컴포넌트스캔에서 제외하기 위해 설정했다.
          */
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class),
+        // basePackages의 하위 패키지를 모두 탐색한다.
+        basePackages = "hello.core.member"
+
 )
 
 public class AutoAppConfig {

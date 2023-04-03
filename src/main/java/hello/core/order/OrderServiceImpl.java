e90faae4ff73b5232430d106719e90047a0e53a9
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-04-03 17:46:57 +0900

    Etc : 다양한 의존관계 주입 방법
    생성자 주입 , 수정자 주입, 필드 주입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -17,6 +17,29 @@ public class OrderServiceImpl implements OrderService{
      */
 
     private final DiscountPolicy discountPolicy;
+
+    /* 필드 주입
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private DiscountPolicy discountPolicy;
+    */
+
+    /*수정자 주입
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+
+        this.discountPolicy = discountPolicy;
+    }
+    */
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
